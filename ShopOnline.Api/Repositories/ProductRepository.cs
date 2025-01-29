c89b7bb448c29f6eca945abd6ecee1b3f60a6f34
robim16 <luis.hoyos@dydsoluciones.net>
2025-01-29 15:52:03 -0500

    creacion del product repository

---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            
+        }
+        public Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
